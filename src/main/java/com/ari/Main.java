ef61d9f77943ee974d83c17a7e42aff324d9f34a
Ahmad Asroruddin <asroruddinahmad09@gmail.com>
2024-05-09 11:13:41 +0700

    let user input value

--- src/main/java/com/ari/Main.java
+++ src/main/java/com/ari/Main.java
@@ -1,9 +1,26 @@
 package com.ari;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         SortingChar sortingChar = new SortingChar();
-        sortingChar.sortingCharacter(new String[]{"Abc", "bCd"});
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Berapa kata yang ingin anda masukan ?");
+        int wordLong = scanner.nextInt();
+        scanner.nextLine();
+        String[] word = new String[wordLong];
+
+                System.out.println("Tulis kata di bawah ini");
+        for(int i = 0 ; i < wordLong; i++){
+            System.out.print("kata ke-" + i +" : ");
+            word[i] = scanner.nextLine();
+        }
+
+        sortingChar.sortingCharacter(word);
     }
 
 }
